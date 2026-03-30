85bbcaeaa7ba4e19648619893b7a84296db1353f
keyona-rerev <keyona@rerev.io>
2026-03-30 10:54:47 -0500

    Update API_KEY in config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Set your Railway API key here before deploying
 const CONFIG = {
   API_BASE: "https://super-connector-api-production.up.railway.app",
-  API_KEY: "REPLACE_WITH_YOUR_SC_API_KEY",
+  API_KEY: "sc_live_k3y_2026_scak",
 };
